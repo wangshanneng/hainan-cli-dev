14e72fabe9c843337301805f5d42bf09477a2948
小王匠 <wsncoder@163.com>
2024-01-05 09:15:54 +0800

    feat：检查Node版本号

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -3,10 +3,29 @@
 module.exports = core;
 
 const pkg = require("../package.json");
+const semver = require("semver");
 const log = require('@hainan-cli-dev/log');
 
+const LOWEST_NODE_VERSION = '12.0.0';
+
 function core() {
-  checkPkgVersion();
+  try {
+    checkPkgVersion();
+    checkNodeVersion();
+  } catch (error) {
+    log.error(error.message);
+  }
+}
+
+// 检查Node版本号
+function checkNodeVersion() {
+  const currentVersion = process.version;
+  const lowestVersion = LOWEST_NODE_VERSION;
+  if (!semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(
+      `hainan-cli-dev 需要安装 v${lowestVersion} 以上版本的 Node.js`
+    )
+  }
 }
 
 // 检查版本号
