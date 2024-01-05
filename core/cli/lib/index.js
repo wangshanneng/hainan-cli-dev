619a6eebbb70e20a1ffafcd6eae09471e37cb65d
小王匠 <wsncoder@163.com>
2024-01-05 22:00:12 +0800

    feat：检查用户名 检查root账户

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,19 +2,40 @@
 
 module.exports = core;
 
-const pkg = require("../package.json");
+const color = require("colors/safe");
 const semver = require("semver");
-const log = require('@hainan-cli-dev/log');
+const userHome = require("user-home");
+const pathExists = require("path-exists").sync;
 
-const LOWEST_NODE_VERSION = '12.0.0';
+const log = require("@hainan-cli-dev/log");
+
+const pkg = require("../package.json");
+
+const LOWEST_NODE_VERSION = "12.0.0";
 
 function core() {
   try {
     checkPkgVersion();
     checkNodeVersion();
+    checkRoot();
+    checkUserHome();
   } catch (error) {
     log.error(error.message);
   }
+}
+
+// 检查用户名
+function checkUserHome() {
+  console.log(userHome);
+  if (!userHome || !pathExists(userHome)) {
+    throw new Error(color.red("当前登录用户主目录不存在！"));
+  }
+}
+
+// 检查Root账户
+function checkRoot() {
+  const rootCheck = require("root-check");
+  rootCheck();
 }
 
 // 检查Node版本号
@@ -24,7 +45,7 @@ function checkNodeVersion() {
   if (!semver.gte(currentVersion, lowestVersion)) {
     throw new Error(
       `hainan-cli-dev 需要安装 v${lowestVersion} 以上版本的 Node.js`
-    )
+    );
   }
 }
 
